669f3421eef636d1c61450db90355851376e4952
SaswatD27 <42236940+SaswatD27@users.noreply.github.com>
2018-11-10 00:59:45 +0530

    Update lab9q11.cpp

--- lab9q11.cpp
+++ lab9q11.cpp
@@ -1,13 +1,16 @@
+//include library
 #include<iostream>
 using namespace std;
 int main()
 {
+	//declare one variable and one pointer each of int, char, float, double, bool and long types
 	int i, *j;
 	char c, *d;
 	float f, *g;
 	double x, *y;
 	bool b, *b1;
 	long l, *m;
+	//display their sizes
 	cout<<"The size of "<<endl<<"Int variable - "<<sizeof(i)<<"\tInt Pointer - "<<sizeof(j)<<endl<<"Char Variable - "<<sizeof(c)<<"\tChar Pointer - "<<sizeof(d)<<endl<<"Float Variable - "<<sizeof(f)<<"\tFloat Pointer - "<<sizeof(g)<<endl<<"Double Variable - "<<sizeof(x)<<"\tDouble Pointer - "<<sizeof(y)<<endl<<"Bool Variable - "<<sizeof(b)<<"\tBool Pointer - "<<sizeof(b1)<<endl<<"Long Variable - "<<sizeof(l)<<"\tLong Pointer - "<<sizeof(m);
 	return 0;
 }
