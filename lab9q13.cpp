d002d73e4c43e53a34c17e27896be5486b573a16
SaswatD27 <42236940+SaswatD27@users.noreply.github.com>
2018-11-10 01:08:03 +0530

    Update lab9q13.cpp

--- lab9q13.cpp
+++ lab9q13.cpp
@@ -1,7 +1,10 @@
+//include library
 #include<iostream>
 using namespace std;
+//write the main function
 int main()
 {
+	//declare an int array A[] of size 10, and int pointer p and an int counter variable i and ask for the input of A
 	int A[10], *p, i;
 	cout<<"Enter 10 integers "<<endl;
 	for(int i=0;i<10;i++)
@@ -9,16 +12,18 @@ int main()
 		cout<<"Integer no. "<<i+1<<" - ";
 		cin>>A[i];
 	}
+	//print values of A using the index method (using a counter variable to traverse through the indices using a for loop)
 	cout<<endl<<"Printing using the index method "<<endl;
 	for(i=0;i<10;i++)
 	{
 		cout<<A[i]<<" ";
 	}
+	//print values of A using the pointer method, i.e. run a no. of iterations equal to the size of A and initially point the pointer p at the first element of A and print the value pointed to by p with each iteration and increment p by 1 for each iteration
 	cout<<endl<<"Printing using the pointer method "<<endl;
 	for(i=0,p=A;i<10;i++)
 	{
 		cout<<*p<<" ";
-		p=&A[i+1];
+		p++;
 	}
 	return 0;
 }		
