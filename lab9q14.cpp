8bb04af76322b5c10ff48eae8fb778444d758e82
SaswatD27 <42236940+SaswatD27@users.noreply.github.com>
2018-11-10 01:13:35 +0530

    Update lab9q14.cpp

--- lab9q14.cpp
+++ lab9q14.cpp
@@ -1,19 +1,24 @@
+//include library
 #include<iostream>
 using namespace std;
+//write the main function
 int main()
 {
+	// declare a string st and ask user to input a name to be stored in st
 	string st;
 	cout<<"Enter your name - ";
 	cin>>st;
+	//Print st using the good ol' index method i.e. using a counter variable and a for loop, run the loop a no. of times equal to the size of the string and print the value of string by accessing its elements using their indices 
 	cout<<"Printing using index method - ";
 	for(int i=0;st[i]!='\0';i++)
 	cout<<st[i];
+	//print st using the pointer method i.e. declare a char pointer p and point it to the first element of st and run a no. of iterations equal to the size of the string and print the value pointed to by p and increment p with each iteration
 	cout<<endl<<"Printing using pointers - ";
 	char *p=&st[0];
 	for(int i=0;st[i]!='\0';i++)
 	{
 		cout<<*p;	
-		p=&st[i+1];
+		p++;
 	}
 	return 0;
 }
