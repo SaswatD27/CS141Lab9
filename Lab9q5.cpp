bb06d352028d9cad4ecf2e335d67df9104a69451
SaswatD27 <42236940+SaswatD27@users.noreply.github.com>
2018-11-10 00:33:17 +0530

    Update Lab9q5.cpp

--- Lab9q5.cpp
+++ Lab9q5.cpp
@@ -1,12 +1,15 @@
+//include libraries
 #include<iostream>
 #include<cstring>
 using namespace std;
+//write the driver function
 int main()
 {
 	char s[10] = "abcde";
 	char* cptr;
-	int i;
+	//point cptr at the last element of s
 	cptr=&s[strlen(s)-1];
+	//for strlen(s)-1 times, run a for loop printing the value pointed to by cptr, decrementing cptr by 1 with each iteration
 	for(int j=strlen(s)-1;j>=0;j--,cptr--)
 	{
 		cout<<*cptr;
