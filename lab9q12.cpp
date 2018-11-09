c8cff37f56af62a7ca5d6959efea145dc936fb48
SaswatD27 <42236940+SaswatD27@users.noreply.github.com>
2018-11-10 01:01:56 +0530

    Update lab9q12.cpp

--- lab9q12.cpp
+++ lab9q12.cpp
@@ -1,12 +1,17 @@
+//include library
 #include<iostream>
 using namespace std;
 int main()
 {
+	//declare int variables a and b and the int pointer p
 	int a,b,*p;
+	//ask for input of a
 	cout<<"Enter an integer value - ";
+	//follow assignment instructions given in the question
 	cin>>a;
 	p=&a;
 	b=*p;
+	//display the values
 	cout<<"a = "<<a<<endl<<"b = "<<b<<endl<<"Value pointed to by p = "<<*p;
 	return 0;
 }
